bde435c02f9a2877bfeb4739f06fbcb4cc6df824
gabriel-dias-clear <gabriel.dias.clear@gmail.com>
2023-08-10 10:28:28 -0300

    insert crud in backend application

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -1,27 +1,61 @@
-const express = require('express');
-const restify = require('restify-clients')
-const router = express.Router();
+var express = require('express');
+var assert = require('assert');
+var restify = require('restify-clients');
+var router = express.Router();
 
-const client = restify.createJsonClient({
+// Creates a JSON client
+var client = restify.createJsonClient({
   url: 'http://localhost:4000'
-})
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   
-  client.get('/users', function(err, request, response, obj){
+  client.get('/users', function(err, request, response, obj) {
+    assert.ifError(err);
+  
+    res.json(obj);
+  });
 
-    try {
+});
 
-    res.end(JSON.stringify(obj, null, 2))
+router.get('/:id', function(req, res, next) {
+  
+  client.get(`/users/${req.params.id}`, function(err, request, response, obj) {
+    assert.ifError(err);
+  
+    res.json(obj);
+  });
 
-      
-    } catch (error) {
-      console.log(error)
-    }
+});
 
+router.put('/:id', function(req, res, next) {
+  
+  client.put(`/users/${req.params.id}`, req.body, function(err, request, response, obj) {
+    assert.ifError(err);
+  
+    res.json(obj);
+  });
 
-  })
+});
+
+router.delete('/:id', function(req, res, next) {
+  
+  client.del(`/users/${req.params.id}`, function(err, request, response, obj) {
+    assert.ifError(err);
+  
+    res.json(obj);
+  });
+
+});
+
+router.post('/', function(req, res, next) {
+  
+  client.post(`/users`, req.body, function(err, request, response, obj) {
+    assert.ifError(err);
+  
+    res.json(obj);
+  });
 
 });
 
